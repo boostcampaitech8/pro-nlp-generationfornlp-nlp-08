cd761a4dce06eeb5f6d21b47450b553fb4db733c
2eeg <fred0110@naver.com>
2026-01-02 09:10:27 +0000

    📦 backup:

--- src/agent/nodes/base.py
+++ src/agent/nodes/base.py
@@ -20,21 +20,22 @@ class BaseLLMNode:
     """
 
     def __init__(self, cfg: DictConfig, model_name: str = "main_solver"):
-        self.factory = ModelFactory(cfg.model)
-        self.model, self.tokenizer = self.factory.get_model(model_name)
         if model_name in cfg.model:
+            self.factory = ModelFactory(cfg.model)
+            self.model, self.tokenizer = self.factory.get_model(model_name)
             self.gen_params = cfg.model[model_name].generation
         else: 
             raise ValueError(f"❌ [No such model] '{model_name}'에 해당하는 모델 설정이 cfg.model에 없습니다.")
 
     @traceable(name="BaseLLMNode.generate")
-    def generate(self, user_prompt: str, system_prompt: str = '', **kwargs) -> str:
+    def generate(self, user_prompt: str, system_prompt: str = '', enable_thinking: bool = False, **kwargs) -> str:
         """
         LLM에게 프롬프트를 입력하고, 생성된 텍스트(답변)를 반환하는 함수
 
         Args:
             user_prompt (str): 사용자 메시지 (실제 질문이나 요청 내용)
             system_prompt (str): 시스템 메시지 (모델에게 역할을 지시하는 용도)
+            enable_thinking (bool): '생각하는' 프롬프트 기법 활성화 여부
             **kwargs: user_prompt 내에 포맷팅할 변수들
         Returns:
             str: 모델이 생성한 순수 텍스트 답변 (특수 토큰 제외)
@@ -57,21 +58,18 @@ class BaseLLMNode:
             messages,
             tokenize=False,
             add_generation_prompt=True,
+            enable_thinking=enable_thinking
         )
         
         inputs = self.tokenizer(text, return_tensors="pt").to(self.model.device)
         
-        # 4. 모델 추론 (Inference)
-        # torch.no_grad()를 사용하여 불필요한 그라디언트 계산을 막아 메모리를 아낍니다.
         with torch.no_grad():
             outputs = self.model.generate(
                 **inputs,
-                **self.gen_params,  # YAML에 설정된 top_p, temp 등이 여기서 적용됨
+                **self.gen_params,
                 streamer = TextStreamer(self.tokenizer, skip_prompt = True),
             )
-            
-        # 5. 디코딩 (Decoding)
-        # 입력으로 넣은 프롬프트 길이를 계산하여, 새로 생성된 뒷부분만 잘라냅니다.
+        
         input_len = inputs["input_ids"].shape[1]
         generated_tokens = outputs[0][input_len:]
         
